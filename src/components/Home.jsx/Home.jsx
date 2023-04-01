1d0b326d999048af71c1873c486ae980204c63c1
Al Hasanul Banna <tutul5800@gmail.com>
2023-04-01 23:25:42 +0600

    first commit

---
+++ src/components/Home.jsx/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+      <div>
+        <h4>Hello</h4>
+        <div>
+          <h2>Card</h2>
+          <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="..." />
+            <div class="card-body">
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
